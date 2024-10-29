4560cc66cb83d9e3357e50213121628a12cbc66d
aldyakbar0201 <aldyhidayatulakbar0201@gmail.com>
2024-10-29 16:12:01 +0700

    first commit

---
+++ introduction.js
@@ -0,0 +1,19 @@
+// frist js node
+
+console.log("helo world");
+console.log(1 + 1);
+
+// batas variable //
+//global scope
+{
+// lv. 1 scope
+    {
+    // lv.2 scope
+    var age1 = 25
+    let age2 = 50
+    const age3 = 70 
+    console.log(age1);
+    }
+}
+
+
